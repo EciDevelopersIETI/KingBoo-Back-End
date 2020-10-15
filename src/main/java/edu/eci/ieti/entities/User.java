1b8a339feaafb1bfe746aac283224d3683ef85e1
fernando-b15 <53972521+fernando-b15@users.noreply.github.com>
2020-10-15 01:06:16 -0500

    Update User.java

--- src/main/java/edu/eci/ieti/entities/User.java
+++ src/main/java/edu/eci/ieti/entities/User.java
@@ -23,7 +23,7 @@ public class User {
 				this.password = password;
 				this.telefono = telefono;
 				this.provider = provider;
-				this.rol = provider.getNit() != null ? "pro":"cli";
+				this.rol = provider != null ? "pro":"cli";
 			}
 			
 			public String getRol() {
